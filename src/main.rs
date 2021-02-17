0b9e02c93af0a295ff8d6271f3706da8bae84c73
DefunctLizard <grantshandy@gmail.com>
2021-02-17 15:04:25 -0700

    clean 'er up

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::{crate_version, App, Arg};
+use colored::Colorize;
 use thesaurus::synonym;
 
 fn main() {
@@ -19,32 +20,44 @@ fn main() {
                 .short("v")
                 .help("Prints synonyms vertically")
                 .takes_value(false)
-                .required(false)
+                .required(false),
         )
         .get_matches();
 
-        let word = match matches.value_of("WORD") {
-            Some(data) => data,
-            None => {
-                eprintln!("ERROR: NO WORD FOUND! EXTREMELY UNEXPECTED!");
-                "error"
-            },
-        };
+    let word = match matches.value_of("WORD") {
+        Some(data) => data,
+        None => {
+            eprintln!("{} {} wasn't detected by clap, but was still let through. This is extremely unexpected.\n", "error:".red().bold(), "<WORD>".yellow());
+            eprintln!("USAGE:\n    thes [FLAGS] <WORD>");
+            eprintln!("\nFor more information try {}", "--help".green());
+            "error"
+        }
+    };
 
-        match synonym(word) {
-            Ok(data) => {
-                let mut synonyms = String::new();
+    match synonym(word) {
+        Ok(data) => {
+            let mut synonyms = String::new();
 
-                for synonym in data.synonyms.iter() {
-                    if matches.is_present("vertical") {
-                        synonyms.push_str(&format!("{}\n", synonym));
-                    } else {
-                        synonyms.push_str(&format!(" {}", synonym));
-                    };
+            for synonym in data.synonyms.iter() {
+                if synonym == word {
+                    continue;
                 };
 
-                println!("{}", synonyms);
-            },
-            Err(error) => eprintln!("Error: {}", error),
-        };
+                if matches.is_present("vertical") {
+                    synonyms.push_str(&format!("{}\n", synonym));
+                } else {
+                    synonyms.push_str(&format!("{} ", synonym));
+                };
+            }
+
+            synonyms = (&synonyms[0..synonyms.len() - 1]).to_string();
+
+            println!("{}", synonyms);
+        }
+        Err(error) => {
+            eprintln!("{} {}\n", "error:".red().bold(), error);
+            eprintln!("USAGE:\n    thes [FLAGS] <WORD>");
+            eprintln!("\nFor more information try {}", "--help".green());
+        }
+    };
 }
